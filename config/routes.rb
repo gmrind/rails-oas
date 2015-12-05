d73438a7da20867dbac01293f02eb44e4e5c862f
gmrind <murtazarind.11@gmail.com>
2015-12-06 04:03:42 +0500

    Devise User model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  devise_for :users
   root              'pages#home'
   get 'about'    => 'pages#about'
   get 'contact'  => 'pages#contact'
